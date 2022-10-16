593778c940fb48e1f18f2389e282249b39e6adb4
Ketro Sithole <101246585+KetroSithole@users.noreply.github.com>
2022-10-16 17:43:28 +0200

    jj

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
-const carouselContainer = document.querySelector(".carouselContainer");
-const slides = document.querySelectorAll('.slides');
-const next = document.querySelector('#next');
-const prev = document.querySelector('#prev');
-let counter = 1;
-const size = slides[0].clientWidth;
-carouselContainer.style.transform = 'translateX(' + (-size * counter) + 'px';
-next.addEventListener('click', () => {
-    if (counter >= slides.length) return;
-    carouselContainer.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    carouselContainer.style.transform = 'translateX(' + (-size * counter) + 'px';
-});
-prev.addEventListener('click', () => {
-    if (counter <= 0) return;
-    carouselContainer.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
-    carouselContainer.style.transform = 'translateX(' + (-size * counter) + 'px';
-});
-carouselContainer.addEventListener('transitionend', () => {
-    console.log(slides[counter]);
-    if (slides[counter].id === 'lastslide') {
-        carouselContainer.style.transition = 'none';
-        counter = slides.length - 2;
-        carouselContainer.style.transform = 'translateX(' + (-size * counter) + 'px';
+let sliderImages = document.querySelectorAll(".slide"),
+    arrowLeft = document.querySelector("#arrow-left"),
+    arrowRight = document.querySelector("#arrow-right"),
+    current = 0;
+
+// Clear all images
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
     }
-});
-carouselContainer.addEventListener('transitionend', () => {
-    console.log(slides[counter]);
-    if (slides[counter].id === 'firstslide') {
-        carouselContainer.style.transition = 'none';
-        counter = slides.length - counter;
-        carouselContainer.style.transform = 'translateX(' + (-size * counter) + 'px';
+}
+
+// Initial slide
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+}
+
+// Show previous
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+}
+
+// Show next
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", function () {
+    if (current === 0) {
+        current = sliderImages.length;
     }
-});  
+    slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function () {
+    if (current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+});
+
+startSlide();
